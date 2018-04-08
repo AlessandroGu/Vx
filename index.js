b49ff3d83972fe15b6ad6a5443df23313788bc93
AlessandroGu <924291390@qq.com>
2018-04-08 17:03:39 +0800

    fourth commit

--- index.js
+++ index.js
@@ -130,3 +130,100 @@ mapGetters({
   // 映射 `this.doneCount` 为 `store.getters.doneTodosCount`
   doneCount: 'doneTodosCount'
 })
+
+// 提交载荷（Payload）
+// 你可以向 store.commit 传入额外的参数，即 mutation 的 载荷（payload）：
+
+// ...
+mutations: {
+  increment (state, n) {
+    state.count += n
+  }
+}
+store.commit('increment', 10)
+// 在大多数情况下，载荷应该是一个对象，这样可以包含多个字段并且记录的 mutation 会更易读：
+
+// ...
+mutations: {
+  increment (state, payload) {
+    state.count += payload.amount
+  }
+}
+store.commit('increment', {
+  amount: 10
+})
+
+
+// 对象风格的提交方式
+// 提交 mutation 的另一种方式是直接使用包含 type 属性的对象：
+
+store.commit({
+  type: 'increment',
+  amount: 10
+})
+// 当使用对象风格的提交方式，整个对象都作为载荷传给 mutation 函数，因此 handler 保持不变：
+
+mutations: {
+  increment (state, payload) {
+    state.count += payload.amount
+  }
+}
+
+
+
+// 在组件中提交 Mutation
+// 你可以在组件中使用 this.$store.commit('xxx') 提交 mutation，或者使用 mapMutations 辅助函数将组件中的 methods 映射为 store.commit 调用（需要在根节点注入 store）。
+
+import { mapMutations } from 'vuex'
+
+export default {
+  // ...
+  methods: {
+    ...mapMutations([
+      'increment', // 将 `this.increment()` 映射为 `this.$store.commit('increment')`
+
+      // `mapMutations` 也支持载荷：
+      'incrementBy' // 将 `this.incrementBy(amount)` 映射为 `this.$store.commit('incrementBy', amount)`
+    ]),
+    ...mapMutations({
+      add: 'increment' // 将 `this.add()` 映射为 `this.$store.commit('increment')`
+    })
+  }
+}
+
+
+
+// Action
+// Action 类似于 mutation，不同在于：
+
+// Action 提交的是 mutation，而不是直接变更状态。
+// Action 可以包含任意异步操作。
+// 让我们来注册一个简单的 action：
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  },
+  actions: {
+    increment (context) {
+      context.commit('increment')
+    }
+  }
+})
+// Action 函数接受一个与 store 实例具有相同方法和属性的 context 对象，因此你可以调用 context.commit 提交一个 mutation，或者通过 context.state 和 context.getters 来获取 state 和 getters。当我们在之后介绍到 Modules 时，你就知道 context 对象为什么不是 store 实例本身了。
+
+// 实践中，我们会经常用到 ES2015 的 参数解构 来简化代码（特别是我们需要调用 commit 很多次的时候）：
+
+actions: {
+  increment ({ commit }) {
+    commit('increment')
+  }
+}
+
+
+//个人认为，state就是保存数据的地方，mutation和methods差不多的作用(mutation必须是同步)，getter和computed的作用差不多，actions是mutation的异步调用。
